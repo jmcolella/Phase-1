2685e1f986307c67d510765c29403929f95197ed
jmcolella <colella.john@gmail.com>
2016-04-11 23:51:23 -0400

    messed up the branches, i deleted the solution accidently

--- week-1/99-bottles-challenge/bottle_song_lyrics.rb
+++ week-1/99-bottles-challenge/bottle_song_lyrics.rb
@@ -3,22 +3,4 @@
 
 def bottle_song_lyrics(n)
   "The lyrics to #{n} Bottles of Beer"
-  counter = n
-  while counter > 2
-    p "#{counter} bottles of beer on the wall, #{counter} bottles of beer. Take one down, pass it around, #{counter - 1} bottles of beer on the wall!"
-    counter -= 1
-  end
-  while counter > 1
-    p "#{counter} bottles of beer on the wall, #{counter} bottles of beer. Take one down, pass it around, #{counter - 1} bottle of beer on the wall!"
-    counter -=1
-  end
-  while counter > 0
-    p "#{counter} bottle of beer on the wall, #{counter} bottle of beer. Take one down, pass it around, no more bottles of beer on the wall!"
-    counter -= 1
-  end
 end
-
-# Driver Code
-
-bottle_song_lyrics(5)
-
